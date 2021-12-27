67b95b11e83d1303ca72e6911db6542f3e4cb878
Josh Simmons <joshsimmons@Joshs-Mac-mini.local>
2021-12-27 16:24:02 -0700

    scroll behavior

--- src/components/atoms/Scroller.jsx
+++ src/components/atoms/Scroller.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import styles from './Scroller.module.scss';
 
 export default function Scroller() {
+  const handleClick = () => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      left: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <button className={styles.scroller} tabIndex={0}>
+    <button className={styles.scroller} tabIndex={0} onClick={handleClick}>
       <p>Scroll to bottom</p>
       <img src={`${process.env.PUBLIC_URL}/down.gif`} alt="down gif" />
     </button>
